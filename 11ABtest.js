d42e7c9ce322afb615614b83dad4c2e3e7433d92
Garen Liang <ideabreeze@gmail.com>
2017-01-28 22:57:28 -0800

    Update 11ABtest.js

--- 11ABtest.js
+++ 11ABtest.js
@@ -1,3 +1,6 @@
+//This challenge requires you to determine if the characters "a" and "b" are separated by exactly 3 places anywhere 
+//in the string. For example, the string "a4 cb" should return true, but the string "aj44ikb" should return false. 
+//A simple loop through the string provides us with a possible solution to the challenge.
 function ABCheck(str) { 
 
   // convert the whole string to lowercase letters
