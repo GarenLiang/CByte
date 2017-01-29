1f032dd3be58c29ecdb82e76266c8b3b7d79fee5
Garen Liang <ideabreeze@gmail.com>
2017-01-28 23:19:05 -0800

    Update 13ExOh.js

--- 13ExOh.js
+++ 13ExOh.js
@@ -19,3 +19,32 @@ function WordCount(str) {
 }
    
 WordCount("xooox");  
+
+
+
+
+
+function WordCount(str) { 
+
+  // split the string into an array
+  var arr = str.split("");
+
+  // create a new array of x's by using the filter function
+  // which takes a test function with 3 parameters:
+  // the element, its index, and the array it came from
+  // and if the test passes it adds this element to the new array
+  var x = arr.filter(function(elem, index, self) { 
+    return elem === 'x';
+  });
+
+  // because we know the string can only contains x's and o's
+  // we can subtract the length of our new array of x's from 
+  // arr and that will leave us with the number of o's
+  var o = arr.length - x.length;
+
+  // see if their lengths are equal
+  return x.length === o;
+         
+}
+   
+WordCount("xooox"); 
