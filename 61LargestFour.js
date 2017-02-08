edf09969444680900a8773896c1377f176080b8b
Garen Liang <ideabreeze@gmail.com>
2017-02-07 21:03:49 -0800

    Create 61LargestFour.js

---
+++ 61LargestFour.js
@@ -0,0 +1,17 @@
+/*
+Have the function LargestFour(arr) take the array of integers stored in arr, and find the four largest elements and
+return their sum. For example: if arr is [4, 5, -2, 3, 1, 2, 6, 6] then the four largest elements in this array are 6, 6, 4, 
+and 5 and the total sum of these numbers is 21, so your program should return 21. 
+If there are less than four numbers in the array your program should return the sum of all the numbers in the array. 
+
+*/
+
+function LargestFour(arr) { 
+
+  // code goes here  
+  arr.sort((a,b)=>a-b).slice(-4).reduce((a,b)=>a+b,0);
+         
+}
+   
+// keep this function call here 
+LargestFour(readline());
